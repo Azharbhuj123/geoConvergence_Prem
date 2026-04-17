30ae4e771089c1b6f53229cc619fa3a470bd26b5
Basit Karim <basit.karimkarim@gmail.com>
2026-04-17 19:05:52 +0500

    Added Setup for Theme

---
+++ frontend/src/app/providers.jsx
@@ -0,0 +1,10 @@
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from '../lib/queryClient'
+
+export const Providers = ({ children }) => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+    </QueryClientProvider>
+  )
+}
