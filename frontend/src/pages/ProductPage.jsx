7908f624c0cbe371f42a07c6755063290c55d763
Junaid <basit.karimkarim@gmail.com>
2026-05-21 00:40:19 +0500

    //

--- frontend/src/pages/ProductPage.jsx
+++ frontend/src/pages/ProductPage.jsx
@@ -118,7 +118,7 @@ export default function ProductPage() {
                                     viewport={{ once: true }}
                                     transition={{ duration: 0.8, ease: [0.23, 1, 0.32, 1] }}
                                     className="rounded-3xl p-8 lg:p-12 
-                               shadow-xl border-2 border-white 
+                               shadow-[0_0_40px_rgba(15,23,42,0.12)] border-2 border-white 
                                mt-16"
                                 >
                                     <Services_Description
